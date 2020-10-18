f8aec9caa613d35606b8c8c1aaa36905f76f6e12
aprilhoward <apehow111@gmail.com>
2020-10-18 18:07:06 -0400

    Initial commit

---
+++ src/Email.java
@@ -0,0 +1,27 @@
+public class Email {
+    // use encapsulation so outside classes can't access directly
+    // we'll access this class through our class API's
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor to receive the firstname and lastName
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Ask for the department
+
+    // Generate a random password
+
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
+}
